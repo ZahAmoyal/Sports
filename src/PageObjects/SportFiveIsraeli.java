9d2c6b2624a79f235747ec9d6c54b5767e5e5209
ZahAmoyal <127040891+ZahAmoyal@users.noreply.github.com>
2023-08-13 09:06:04 +0300

    Sports 13/08

--- src/PageObjects/SportFiveIsraeli.java
+++ src/PageObjects/SportFiveIsraeli.java
@@ -11,6 +11,6 @@ public class SportFiveIsraeli extends BasePage {
     }
 
     public void sportFiveIsraeli() throws InterruptedException {
-        SportFiveArticles(urlIsraeliSportFive,1);
+        SportFiveArticles(urlIsraeliSportFive,0);
     }
 }
